898838ed60f23b0d2b387522c2f3c8c85bb9c461
mark8773 <158565946+mark8773@users.noreply.github.com>
2025-01-09 11:26:15 +0200

    1

--- gpkg/mesa/build.sh
+++ gpkg/mesa/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="An open-source implementation of the OpenGL specificatio
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="24.2.6"
+TERMUX_PKG_VERSION="24.2.8"
 TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/mesa-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=2b68c4a6f204c1999815a457299f81c41ba7bf48c4674b0b2d1d8864f41f3709
 TERMUX_PKG_DEPENDS="libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
@@ -15,35 +15,26 @@ TERMUX_PKG_PYTHON_COMMON_DEPS="mako, setuptools, pyyaml"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -D android-libbacktrace=disabled
 -D b_ndebug=true
--D dri3=enabled
--D egl=enabled
--D gallium-opencl=icd
--D gallium-drivers=freedreno,swrast,virgl,zink,r600,radeonsi,nouveau,lima,panfrost
--D gallium-extra-hud=true
--D gallium-nine=true
--D gallium-va=enabled
--D gallium-vdpau=enabled
--D gallium-omx=bellagio
+-D egl=disabled
+-D gallium-drivers=
 -D gallium-xa=disabled
--D gbm=enabled
--D gles1=enabled
--D gles2=enabled
--D glvnd=true
--D glx=dri
--D intel-clc=system
+-D gbm=disabled
+-D gles1=disabled
+-D gles2=disabled
+-D glvnd=false
 -D libunwind=disabled
 -D llvm=enabled
 -D microsoft-clc=disabled
--D osmesa=true
--D platforms=x11,wayland
--D shared-glapi=enabled
+-D osmesa=false
+-D platforms=x11
+-D shared-glapi=disabled
 -D valgrind=disabled
--D vulkan-layers=device-select,overlay
+-D vulkan-layers=
 "
 
 termux_step_pre_configure() {
 	case $TERMUX_ARCH in
-		arm|aarch64) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast,panfrost,freedreno -Dfreedreno-kmds=msm,kgsl";;
+		arm|aarch64) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl";;
 		*) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast";;
 	esac
 	export MESON_PACKAGE_CACHE_DIR="${TERMUX_PKG_SRCDIR}"
